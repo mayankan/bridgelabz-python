e769c76617564350b49cdcc5e974b4bc3be686af
Mayank Anand <mayankan@gmail.com>
2022-02-16 18:18:44 +0530

    Minor Changes in Q2

--- q2.py
+++ q2.py
@@ -1,7 +1,8 @@
 import random
 noOfTimes = 0
-while int(noOfTimes) < 1:
-    noOfTimes = input("Enter the number of times to Flip Coin ")
-output = random.choices([0, 1], k=int(noOfTimes))
-percentage = (sum(output)/int(noOfTimes))
+while noOfTimes < 1:
+    noOfTimes = input("Enter the number of times to Flip Coin: ")
+    noOfTimes = int(noOfTimes)
+output = random.choices([0, 1], k=noOfTimes)
+percentage = (sum(output)/noOfTimes)
 print(percentage)
