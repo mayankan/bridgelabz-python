cadafee6ba71c8cab997bea657ffbbf5d248e21b
Mayank Anand <mayankan@gmail.com>
2022-02-21 17:02:36 +0530

    Correcting and Completing Gambler Program

--- logical_programs/gambler.py
+++ logical_programs/gambler.py
@@ -1,31 +1,30 @@
+# Import random package to get random 0 or 1 which means user won or lost the bet.
 import random
 
 
 def gambling(stake, goal, trials):
     """
-    Does Gambling for a user with given stake, a goal amount to reach and provided no. of trials and returns no. of wins and percentage of wins and losses.
-    :param stake:
-    :param goal:
-    :param trials:
-    :return:
+    Does Gambling for a user with given stake, a goal amount to reach and provided no. of trials and returns no. of
+    wins and percentage of wins and losses.
+    :param stake: The Initial Stake Amount the user is gambling.
+    :param goal: The Goal Amount till User wants to Gamble.
+    :param trials: The Number of Trials User wants to Gamble.
+    :return: The Number of Wins user won in th gamble.
     """
-    bets = 0
-    wins = 0
-
-    for t in range(trials):
-        cash = stake
-        # The user bets until he/she reaches the goal or gets broke
-        while 0 < cash < goal:
-            bets += 1
-            if random.randrange(0, 2) == 0:
-                cash += 1
-            else:
-                cash -= 1
-        if cash == goal:
-            wins += 1
-    print(str(wins) + ' wins')
-    print(str(100 * wins / trials) + '% wins')
-    print(str((1 - wins / trials) * 100) + '% loss')
+    try:
+        bets = 0
+        wins = 0
+        for times in range(trials):
+            while stake != goal and stake != 0:
+                bets += 1
+                if random.randint(0, 1) == 0:
+                    stake += 1
+                    wins += 1
+                else:
+                    stake -= 1
+        return wins, bets
+    except Exception as e:
+        print("{} is raised.".format(e))
 
 
 def main():
@@ -33,7 +32,10 @@ def main():
         stake = int(input("Enter stake: "))
         goal = int(input("Enter a goal: "))
         trials = int(input("Enter no. of trials: "))
-        gambling(stake, goal, trials)
+        wins, bets = gambling(stake, goal, trials)
+        print(str(wins) + ' wins')
+        print(str(100 * wins / bets) + '% wins')
+        print(str((1 - wins / bets) * 100) + '% loss')
     except Exception as e:
         print("{} is raised.".format(e))
 
