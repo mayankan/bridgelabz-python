1a548b79d15be8a5935c428e63df0149ce9d712f
Mayank Anand <mayankan@gmail.com>
2022-02-22 08:29:22 +0530

    Updating code in stopwatch and implementing exception handling

--- logical_programs/stopwatch.py
+++ logical_programs/stopwatch.py
@@ -1,23 +1,29 @@
 import time
 
 
-def simulate_stopwatch():
+def stopwatch_calculation(start_time, end_time):
     """
-        Simulate Stopwatch - Pressing Enter to start the stopwatch and stop the stopwatch
-        :return: prints the Elapsed time
-        """
-    input("Press Enter to Start")
-    start_time = time.time()
-    input("Press Enter to Stop")
-    end_time = time.time()
-    # Measuring the elapsed time between start and end
-    time_elapsed = end_time - start_time
-    print(round(time_elapsed, 4), " seconds")
+    Returns elapsed time between start time and end time provided by user.
+    :param start_time: Start time provided by user.
+    :param end_time: End time provided by user.
+    :return: End time minus start time rounded to seconds.
+    """
+    try:
+        # Measuring the elapsed time between start and end
+        time_elapsed = end_time - start_time
+        return round(time_elapsed, 4)
+    except Exception as e:
+        print("{} is raised.".format(e))
 
 
 def main():
     try:
-        simulate_stopwatch()
+        input("Press Enter to Start")
+        start_time = time.time()
+        input("Press Enter to Stop")
+        end_time = time.time()
+        time_elapsed = stopwatch_calculation(start_time, end_time)
+        print(time_elapsed, " seconds")
     except Exception as e:
         print("{} is raised.".format(e))
 
