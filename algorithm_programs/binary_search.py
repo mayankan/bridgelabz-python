7bcece98219248ce23a939798bdac5b574ce9e9f
Mayank Anand <mayankan@gmail.com>
2022-03-02 12:21:54 +0530

    Updating Binary Search Program

--- algorithm_programs/binary_search.py
+++ algorithm_programs/binary_search.py
@@ -1,23 +1,23 @@
-def binary_word_search(filename):
+def binary_word_search(filename, search_word):
     """Binary Search the word from word list
     Parameters:
         filename: text file
         word: input word to be searched in the file
     Returns:
         prints whether the word is found or not
+        :param search_word:
     """
     my_list = []
     # Splitting the words and adding it to an empty list
     with open(filename, 'r') as f:
-        for line in f:
-            my_list.extend(line.lower().split())
+        print(f)
+        # for line in f:
+        #     my_list.extend(line.lower().split())
 
     new_list = my_list
-    # Taking input from the user
-    word = input("Enter a word to be searched: ")
     # Traversing through the list to find the input word
     for i in range(len(new_list)):
-        if word == new_list[i]:
+        if search_word == new_list[i]:
             print("The word is found")
             break
     else:
@@ -26,7 +26,9 @@ def binary_word_search(filename):
 
 def main():
     filename = "mytext.txt"
-    binary_word_search(filename)
+    # Taking input from the user
+    word = input("Enter a word to be searched: ")
+    binary_word_search(filename, word)
 
 
 if __name__ == "__main__":
